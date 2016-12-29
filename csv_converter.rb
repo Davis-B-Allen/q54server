e41c42139d30ea92b1ffaa8dfbf53dc1e8eb8bb9
Davis Allen <davis.b.allen@gmail.com>
2016-12-29 16:12:16 -0500

    Update csv converter script to use relative file path and capture first row of CSV as attribute names

--- csv_converter.rb
+++ csv_converter.rb
@@ -1,8 +1,12 @@
 require 'csv'
 require 'json'
 
-keys = ['card', 'category', 'question', 'caption']
+rootdir = File.expand_path("../", __FILE__)
+csv_to_be_converted = rootdir + "/eg_cards.csv"
+cards_json_target = rootdir + "/cards.json"
 
-cards = CSV.read("eg_cards.csv").map {|a| Hash[ keys.zip(a) ] }
+cards_arr = CSV.read(csv_to_be_converted)
 
-IO.write("cards.json", cards.to_json)
+cards = cards_arr[1..cards_arr.length-1].map {|a| Hash[ cards_arr[0].zip(a) ] }
+
+IO.write(cards_json_target, cards.to_json)
